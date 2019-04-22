b41342f82eace1d477f5dcce4bb4283161bb4dc5
NeseAtes <nese.ates97@gmail.com>
2019-04-22 16:08:07 +0300

    ff

--- public/pages/userComponent/userController.js
+++ public/pages/userComponent/userController.js
@@ -1,11 +1,35 @@
 
 app.controller('userController',function($scope,$http,$routeParams) {
+	$scope.tab = 1;
+
+    $scope.setTab = function(newTab){
+      $scope.tab = newTab;
+    };
+
+    $scope.isSet = function(tabNum){
+      return $scope.tab == tabNum;
+    };
+
+
 	$scope.personnel_id=$routeParams.userid;
 	console.log("$scope.personnel_id",$scope.personnel_id)
 	$http.get("http://localhost:3000/api/personnels?personnel_ID="+$scope.personnel_id)
     .then(function(response) {
       console.log("response3",response.data.data);
       $scope.users=response.data.data
+    });
+
+    console.log($scope.personnel_id)
+    $http.get("http://localhost:3000/api/subject/personnel?personnel_ID="+$scope.personnel_id)
+    .then(function(response) {
+      console.log("response4",response);
+      $scope.perSub=response.data.data
+    });
+
+    $http.get("http://localhost:3000/api/comment/personnel?personnel_ID="+$scope.personnel_id)
+    .then(function(response) {
+      console.log("response5",response);
+      $scope.comSub=response.data.data
     });
 
 })
