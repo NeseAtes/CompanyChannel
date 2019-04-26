52998a740bcd11b58fefc5dcaf8fc250b1b55633
nazlikrmn <nazli.karaman456@gmail.com>
2019-04-26 11:39:21 +0300

    token-f

--- controllers/TokenController.js
+++ controllers/TokenController.js
@@ -1,48 +1,43 @@
 var jwt = require('jsonwebtoken');
-var token= function(data){
-    var token = jwt.sign(data, 'companykey');   //private key=> companykey
-    //{expiresIn: 30}
-    return {token:token}
-  }
-  var tokenControl=function(req, res,next) {
-    if(req.cookies.auth!=undefined){
-      var token = req.cookies.auth.token;
-      if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
-          
-      jwt.verify(token, 'companykey', function(err, decoded) {
+var token = function (data) {
+  var token = jwt.sign(data, 'companykey');   //private key=> companykey
+  //{expiresIn: 30}
+  return { token: token }
+}
+
+var tokenControl = function (req, res, next, role) {
+  if (req.cookies.auth != undefined) {
+    var token = req.cookies.auth.token;
+    if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
+    jwt.verify(token, 'companykey', function (err, decoded) {
       if (err) {
         res.clearCookie("auth");
         return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
       }
-        res.locals.data={auth:true,data:decoded};
-        next();
-      });
-    }
-    else{
-      return res.status(503).send({message:'Api not connected'});
-    }
-  }
-  var adminControl=function(req,res,next){
-    if(req.cookies.auth!=undefined){
-      var token = req.cookies.auth.token;
-      if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
-      jwt.verify(token, 'companykey', function(err, decoded) {
-      if (err) {
-        res.clearCookie("auth");
-        return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
-      }
-        if(decoded.role=="admin"){
-          res.locals.data={auth:true,data:decoded};
+      if (role == "admin") {
+        if (decoded.role == "admin") {
+          res.locals.data = { auth: true, data: decoded };
           next();
         }
-        else{
+        else {
           return res.status(500).send({ auth: false, message: 'You are not authorized to access the page.' });
         }
-      });
-    }else{
-      return res.status(503).send({message:'Database not connected'});
-    }
+      }
+      else {
+        res.locals.data = { auth: true, data: decoded };
+        next();
+      }
+    });
+  } else {
+    return res.status(503).send({ message: 'Database not connected' });
   }
-  module.exports.token=token;
-  module.exports.tokenControl=tokenControl;
-  module.exports.adminControl=adminControl;
+}
+var normalControl = function (req, res, next) {
+  tokenControl(req, res, next, "normal");
+}
+var adminControl = function (req, res, next) {
+  tokenControl(req, res, next, "admin");
+}
+module.exports.token = token;
+module.exports.normalControl = normalControl;
+module.exports.adminControl = adminControl;
--- routes/routes.js
+++ routes/routes.js
@@ -2,49 +2,49 @@ var cookieParser = require('cookie-parser');
 var BaseController = require("../controllers/BaseController");
 var CommentController = require("../controllers/CommentController");
 var SubjectController = require("../controllers/SubjectController");
-var CompaniesCtrl= require("../controllers/CompaniesController");
-var PersonnelsCtrl= require("../controllers/PersonnelsController");
-var ElasticSearchCtrl =require("../controllers/ElasticSearchController");
-var HomeCtrl= require("../controllers/HomeController");
-var TokenCtrl=require("../controllers/TokenController");
-var TagCtrl=require("../controllers/TagController");
+var CompaniesCtrl = require("../controllers/CompaniesController");
+var PersonnelsCtrl = require("../controllers/PersonnelsController");
+var ElasticSearchCtrl = require("../controllers/ElasticSearchController");
+var HomeCtrl = require("../controllers/HomeController");
+var TokenCtrl = require("../controllers/TokenController");
+var TagCtrl = require("../controllers/TagController");
 
-module.exports = function(app) {
+module.exports = function (app) {
 	app.use(cookieParser())
-	app.get('/',HomeCtrl.IndexAction);
-	app.post('/login',BaseController.InitSession,PersonnelsCtrl.login,BaseController.EndSession);
+	app.get('/', HomeCtrl.IndexAction);
+	app.post('/login', BaseController.InitSession, PersonnelsCtrl.login, BaseController.EndSession);
 
-	app.post('/api/subject',TokenCtrl.tokenControl,BaseController.InitSession,SubjectController.addSubject,BaseController.EndSession);
-	app.get('/api/subject',TokenCtrl.tokenControl,BaseController.InitSession,SubjectController.getSubject,BaseController.EndSession);
-	app.get('/api/subject/one',TokenCtrl.tokenControl,BaseController.InitSession,SubjectController.getOneSubject,BaseController.EndSession);
-	app.get('/api/subject/tag',TokenCtrl.tokenControl,BaseController.InitSession,SubjectController.getSubjectsforTag,BaseController.EndSession);
-	app.delete('/api/subject/:subject_ID',TokenCtrl.tokenControl, BaseController.InitSession, SubjectController.deleteSubject, BaseController.EndSession);
-	app.get('/api/subject/personnel',TokenCtrl.tokenControl, BaseController.InitSession, SubjectController.getPersonnelSubjects, BaseController.EndSession);
+	app.post('/api/subject', TokenCtrl.normalControl, BaseController.InitSession, SubjectController.addSubject, BaseController.EndSession);
+	app.get('/api/subject', TokenCtrl.normalControl, BaseController.InitSession, SubjectController.getSubject, BaseController.EndSession);
+	app.get('/api/subject/one', TokenCtrl.normalControl, BaseController.InitSession, SubjectController.getOneSubject, BaseController.EndSession);
+	app.get('/api/subject/tag', TokenCtrl.normalControl, BaseController.InitSession, SubjectController.getSubjectsforTag, BaseController.EndSession);
+	app.delete('/api/subject/:subject_ID', TokenCtrl.normalControl, BaseController.InitSession, SubjectController.deleteSubject, BaseController.EndSession);
+	app.get('/api/subject/personnel', TokenCtrl.normalControl, BaseController.InitSession, SubjectController.getPersonnelSubjects, BaseController.EndSession);
 
-	app.post('/api/comment',TokenCtrl.tokenControl,BaseController.InitSession,CommentController.addComment,BaseController.EndSession);
-	app.get('/api/comment',TokenCtrl.tokenControl,BaseController.InitSession,CommentController.getComment,BaseController.EndSession);
-	app.get('/api/comment/personnel',TokenCtrl.tokenControl,BaseController.InitSession,CommentController.getPersonnelComments,BaseController.EndSession);
-	app.post('/api/comment/update',TokenCtrl.tokenControl,BaseController.InitSession,CommentController.updateComment,BaseController.EndSession);
-	app.delete('/api/comment',TokenCtrl.tokenControl,BaseController.InitSession,CommentController.deleteComment,BaseController.EndSession);
-	app.post('/api/comment/answer',TokenCtrl.tokenControl,BaseController.InitSession,CommentController.answer,BaseController.EndSession);
+	app.post('/api/comment', TokenCtrl.normalControl, BaseController.InitSession, CommentController.addComment, BaseController.EndSession);
+	app.get('/api/comment', TokenCtrl.normalControl, BaseController.InitSession, CommentController.getComment, BaseController.EndSession);
+	app.get('/api/comment/personnel', TokenCtrl.normalControl, BaseController.InitSession, CommentController.getPersonnelComments, BaseController.EndSession);
+	app.post('/api/comment/update', TokenCtrl.normalControl, BaseController.InitSession, CommentController.updateComment, BaseController.EndSession);
+	app.delete('/api/comment', TokenCtrl.normalControl, BaseController.InitSession, CommentController.deleteComment, BaseController.EndSession);
+	app.post('/api/comment/answer', TokenCtrl.normalControl, BaseController.InitSession, CommentController.answer, BaseController.EndSession);
 
 
-	app.get('/api/companies',TokenCtrl.tokenControl,BaseController.InitSession,CompaniesCtrl.getCompanies,BaseController.EndSession);
-	app.post('/api/companies',BaseController.InitSession,CompaniesCtrl.addCompany,BaseController.EndSession);
-	app.delete('/api/companies',TokenCtrl.tokenControl,BaseController.InitSession,CompaniesCtrl.deleteCompany,BaseController.EndSession);
+	app.get('/api/companies', TokenCtrl.normalControl, BaseController.InitSession, CompaniesCtrl.getCompanies, BaseController.EndSession);
+	app.post('/api/companies', BaseController.InitSession, CompaniesCtrl.addCompany, BaseController.EndSession);
+	app.delete('/api/companies', TokenCtrl.normalControl, BaseController.InitSession, CompaniesCtrl.deleteCompany, BaseController.EndSession);
 
-	app.get('/api/personnels',TokenCtrl.tokenControl,BaseController.InitSession,PersonnelsCtrl.getPersonnels,BaseController.EndSession);
-	app.post('/api/personnels',TokenCtrl.adminControl,BaseController.InitSession,PersonnelsCtrl.addPersonnel,BaseController.EndSession);
-	app.post('/api/personnels/password',TokenCtrl.tokenControl,BaseController.InitSession,PersonnelsCtrl.updatePassword,BaseController.EndSession);
+	app.get('/api/personnels', TokenCtrl.normalControl, BaseController.InitSession, PersonnelsCtrl.getPersonnels, BaseController.EndSession);
+	app.post('/api/personnels', TokenCtrl.adminControl, BaseController.InitSession, PersonnelsCtrl.addPersonnel, BaseController.EndSession);
+	app.post('/api/personnels/password', TokenCtrl.normalControl, BaseController.InitSession, PersonnelsCtrl.updatePassword, BaseController.EndSession);
 
-	app.post('/api/createIndex',BaseController.InitSession,ElasticSearchCtrl.createIndex,BaseController.EndSession);
+	app.post('/api/createIndex', BaseController.InitSession, ElasticSearchCtrl.createIndex, BaseController.EndSession);
 
-	app.post('/api/elastic',TokenCtrl.tokenControl,BaseController.InitSession,ElasticSearchCtrl.search,BaseController.EndSession);
-	app.delete('/api/elastic/:_id',TokenCtrl.tokenControl, BaseController.InitSession, ElasticSearchCtrl.deleteDocument, BaseController.EndSession);
+	app.post('/api/elastic', TokenCtrl.normalControl, BaseController.InitSession, ElasticSearchCtrl.search, BaseController.EndSession);
+	app.delete('/api/elastic/:_id', TokenCtrl.normalControl, BaseController.InitSession, ElasticSearchCtrl.deleteDocument, BaseController.EndSession);
 
-	app.post('/api/tags',TokenCtrl.tokenControl,BaseController.InitSession,TagCtrl.addTag,BaseController.EndSession);
-	app.get('/api/tags',TokenCtrl.tokenControl,BaseController.InitSession,TagCtrl.getAllTag,BaseController.EndSession);
-	app.post('/api/tags/delete',TokenCtrl.tokenControl,BaseController.InitSession,TagCtrl.deleteTag,BaseController.EndSession);
+	app.post('/api/tags', TokenCtrl.normalControl, BaseController.InitSession, TagCtrl.addTag, BaseController.EndSession);
+	app.get('/api/tags', TokenCtrl.normalControl, BaseController.InitSession, TagCtrl.getAllTag, BaseController.EndSession);
+	app.post('/api/tags/delete', TokenCtrl.normalControl, BaseController.InitSession, TagCtrl.deleteTag, BaseController.EndSession);
 
-	app.get('/logout',TokenCtrl.tokenControl,BaseController.InitSession,PersonnelsCtrl.logout,BaseController.EndSession);
+	app.get('/logout', TokenCtrl.normalControl, BaseController.InitSession, PersonnelsCtrl.logout, BaseController.EndSession);
 };
