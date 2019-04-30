08ea3be56b221e59b1ce972e87cba66dad5f9f3f
NeseAtes <nese.ates97@gmail.com>
2019-04-30 17:54:40 +0300

    ee

--- public/pages/userComponent/userController.js
+++ public/pages/userComponent/userController.js
@@ -3,6 +3,9 @@ app.config(function($routeProvider,$locationProvider){
     $routeProvider.when('/subject/:subjectid', {
       templateUrl: './pages/subjectComponent/subject.html',
       controller: 'subjectController'
+    }).when('/sifre/:sifreid', {
+      templateUrl: './pages/sifreComponent/sifre.html',
+      controller: 'sifreController'
     });
 });
 
@@ -19,7 +22,7 @@ app.controller('userController',function($scope,$http,$routeParams,$timeout,$win
 
 
 	$scope.personnel_id=$routeParams.userid;
-	console.log("$scope.personnel_id",$scope.personnel_id)
+	console.log("$scope.personnel_id2",$scope.personnel_id)
 	$http.get("http://localhost:3000/api/personnels?personnel_ID="+$scope.personnel_id)
     .then(function(response) {
       console.log("response3",response.data.data);
